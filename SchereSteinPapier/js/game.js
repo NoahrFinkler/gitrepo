c18cb99682b67944049a8ffa9225c81a15dd24c2
NoahrFinkler <noah.finkler@student.htldornbirn.at>
2026-06-17 16:19:21 +0200

    17.06.2026

---
+++ SchereSteinPapier/js/game.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Elemente auswählen
+
+    const choiceButtons = document.querySelectorAll('.choice-btn');
+    const playerScoreEl = document.getElementById('player-score');
+    const computerScoreEl = document.getElementById('computer-score');
+    const playerChoiceEl = document.getElementById('player-choice');
+    const computerChoiceEl = document.getElementById('computer-choice');
+    const roundResultEl = document.getElementById('round-result');
+    const resetButton = document.getElementById('reset-btn');
+});
+
+
+
+
